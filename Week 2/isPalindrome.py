48de732fad02254bbd458b115feb9eaf2812b4b9
MattJ-UK <mattj1984@gmail.com>
2019-09-23 19:25:07 +0100

    isPalindrome refactor

--- Week 2/isPalindrome.py
+++ Week 2/isPalindrome.py
@@ -1,7 +1,6 @@
 def isPalindrome(str):
     if len(str) <=1:
         return True
-    elif str[0] == str[-1]:
-        return isPalindrome(str[1:-1])
     else:
-        return False
+        return isPalindrome(str[1:-1]) and str[0] == str[-1]
+
